e34634c2b8a06bda2431ba5e34182a0684f5105a
Benjamin Peters <bpeters@mpi-sws.org>
2026-04-01 15:05:11 +0200

    simplified example further

--- token_logrel/temp.v
+++ token_logrel/temp.v
@@ -1,20 +1,8 @@
 From token_logrel Require Import temp2.
 
-Notation "|===> Q" := (let _ := token_world in Q) (at level 99).
+Lemma tupd_intro P : P -> |===> P.
+Proof.  Admitted.
 
-Section tupd_proofs.
-  Lemma tupd_intro P : P -> |===> P.
-  Proof.  Admitted.
-End tupd_proofs.
-
-
-(* this works *)
-
-From Stdlib.QArith Require Import Qcanon.
-
-Notation "|===> Q" := (let _ := Qc in Q) (at level 99).
-
-Section tupd_proofs.
-  Lemma tupd_intro_Qc P : P -> |===> P.
-  Proof.  Admitted.
-End tupd_proofs.
+Notation "|===> Q" := (Q) (at level 99).
+Lemma tupd_intro_Qc P : P -> |===> P.
+Proof.  Admitted.
--- token_logrel/temp2.v
+++ token_logrel/temp2.v
@@ -1,1 +1,4 @@
 Definition token_world := unit.
+
+Notation "|===> Q" := (let _ := token_world in Q) (at level 99).
+
