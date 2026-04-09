e53b11d523477a6c3cc977882d1a2068deecdf45
Benjamin Peters <bpeters@mpi-sws.org>
2026-04-09 11:10:11 +0200

    reproduce bug again

---
+++ token_logrel2/temp.v
@@ -0,0 +1,8 @@
+From token_logrel2 Require Import temp2.
+
+Lemma tupd_intro P : P -> |===> P.
+Proof.  Admitted.
+
+Notation "|===> Q" := (Q) (at level 99).
+Lemma tupd_intro_Qc P : P -> |===> P.
+Proof.  Admitted.
---
+++ token_logrel2/temp2.v
@@ -0,0 +1,4 @@
+Definition token_world := unit.
+
+Notation "|===> Q" := (let _ := token_world in Q) (at level 99).
+
